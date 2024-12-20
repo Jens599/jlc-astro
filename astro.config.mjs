c0a3c347ae989f53635b4958c2b585029397a3e4
Jens599 <workjenss@gmail.com>
2024-12-20 14:21:37 +0545

    Update Astro configuration and package.json for deployment settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jens599.github.io',
+  // base: 'jlc-astro',
   integrations: [tailwind(), icon()],
   devToolbar: {
     enabled: true,
