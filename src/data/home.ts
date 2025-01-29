6233f2900384139f645256495403aaf42361750c
Jens599 <workjenss@gmail.com>
2025-01-29 13:35:27 +0545

    Add visa icons and update homepage content - Added new icons. - Updated homepage layout and content in `src/pages/index.astro`.

--- src/data/home.ts
+++ src/data/home.ts
@@ -3,25 +3,70 @@ const translate = '/src/assets/translate.png'
 const plane = '/src/assets/plane.png'
 
 export const homeCards = [
-  {
-    icon: chat,
-    alt: 'chat icon',
-    title: 'Free Consultation',
-    statement:
-      'JLC always provide absolutely transparency and fair Information with Best Guidance for Career.',
-  },
-  {
-    icon: translate,
-    alt: 'translate icon',
-    title: 'Language Classes',
-    statement:
-      'We provide Japanese language Classes to communicate, educate and transform japanese culture.',
-  },
-  {
-    icon: plane,
-    alt: 'plane icon',
-    title: 'Visa And Documentation',
-    statement:
-      'We provide easiest documentation support to study,travel at Japan in any purpose, as a Student Visa, Dependent Visa and Visit Visa.',
-  },
+	{
+		icon: chat,
+		alt: 'chat icon',
+		title: 'Free Consultation',
+		statement:
+			'JLC always provide absolutely transparency and fair Information with Best Guidance for Career.'
+	},
+	{
+		icon: translate,
+		alt: 'translate icon',
+		title: 'Language Classes',
+		statement:
+			'We provide Japanese language Classes to communicate, educate and transform japanese culture.'
+	},
+	{
+		icon: plane,
+		alt: 'plane icon',
+		title: 'Visa And Documentation',
+		statement:
+			'We provide easiest documentation support to study,travel at Japan in any purpose, as a Student Visa, Dependent Visa and Visit Visa.'
+	}
+]
+
+export const services = [
+	{
+		icon: 'Immigrant Visa',
+		alt: 'immigrant visa icon',
+		title: 'Immigrant Visas',
+		statement:
+			'Secure your path to permanent residency in Japan with an easy-to-navigate immigrant visa application process and expert support.'
+	},
+	{
+		icon: 'Local Visa',
+		alt: 'local visa icon',
+		title: 'Local Visa',
+		statement:
+			'Enjoy a seamless entry to Japan with a local visa, perfect for short-term stays, travel, or business opportunities.'
+	},
+	{
+		icon: 'Medical Visa',
+		alt: 'medical visa icon',
+		title: 'Medical Visa',
+		statement:
+			"Access Japan's advanced healthcare system with a medical visa, ensuring you receive top-notch treatment at renowned medical facilities."
+	},
+	{
+		icon: 'Student Visa',
+		alt: 'student visa icon',
+		title: 'Student Visa',
+		statement:
+			'Pursue your educational goals in Japan with a student visa, unlocking opportunities at prestigious universities and research institutions.'
+	},
+	{
+		icon: 'Tourist Visa',
+		alt: 'tourist visa icon',
+		title: 'Tourist Visa',
+		statement:
+			'Explore the beauty and culture of Japan by securing a tourist visa, designed for short-term visits and unforgettable experiences.'
+	},
+	{
+		icon: 'Business Visa',
+		alt: 'business visa icon',
+		title: 'Business Visa',
+		statement:
+			'Expand your business operations in Japan with a business visa, designed for professionals seeking long-term opportunities and partnerships.'
+	}
 ]
